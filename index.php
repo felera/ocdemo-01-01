ae1daf2329d5e9d0432dbe2452063c9e89326247
francisco <elcofla46@hotmail.com>
2020-02-07 19:55:53 -0500

    prueba 2

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<? phpinfo(); ?>
+<html>
+    <head>
+        <?php
+            $author = getenv('AUTHOR');
+            $storage_type = getenv('STORAGE_TYPE');
+
+            echo "<title>PHP info by " . $author . "</title>";
+
+        ?>
+    </head>
+    <body>
+        <?php
+            phpinfo();
+        ?>
+    </body>
+<html>
