5f7067ba061ba8b5c6eefcfc70f3e41944df1208
francisco <felera@users.noreply.github.com>
2020-02-16 07:37:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,15 @@
     <body>
         <h1>-- Hello World! --</h1><br>
         <?php
-            phpinfo();
+            $link = mysqli_connect($_ENV["DATABASE_SERVICE_NAME"],$_ENV["DATABASE_USER"],
+            $_ENV["DATABASE_PASSWORD"],$_ENV["DATABASE_NAME"]);
+             if (!$link) {
+                http_response_code (500);
+                error_log ("Error: unable to connect to database\n");
+             die();
+             }else{
+                http_response_code (200);
+             }
         ?>
     </body>
 <html>
